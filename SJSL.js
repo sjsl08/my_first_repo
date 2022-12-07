8bdc64e47d1542632ee8587e254e2d57e56fe1a3
SJSL <lallsimu80@gmail.com>
2022-12-07 13:34:03 +0530

    initial commit

--- SJSL.js
+++ SJSL.js
@@ -1,1 +1,7 @@
 console.log("hello git");
+console.log("hello git");
+console.log("hello git");
+console.log("hello git");
+console.log("hello git");
+console.log("hello git");
+console.log("hello git");
